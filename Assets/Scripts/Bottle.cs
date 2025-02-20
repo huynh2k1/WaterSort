caedc46a160f5a4e99b17c25eeb724bfc4367dd8
Nguyen Huynh <huynhnv@solarapp.asia>
2025-02-20 22:48:33 +0700

    init project

---
+++ Assets/Scripts/Bottle.cs
@@ -0,0 +1,109 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottle : MonoBehaviour
+{
+    [SerializeField] Transform _mask;
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] Material _waterMat;
+    [SerializeField] SpriteRenderer _spriteRenderer;
+    [SerializeField] AnimationCurve _animationCurve;
+    [Header("Properties")]
+    public float[] thresholdFills = { -0.5f, -0.29f, -0.07f, };
+    public Color[] bottleColors;
+
+    [SerializeField] float time = 2f;
+    [SerializeField] float _scaleOffset = 1f;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void OnMouseDown()
+    {
+        DoNuoc();
+    }
+
+    void Init()
+    {
+        if (_waterMat == null)
+        {
+            _waterMat = _spriteRenderer.material;
+        }
+
+        UpdateColorsOnBottle();
+    }
+
+    private void Update()
+    {
+        if (_waterMat)
+        {
+            _waterMat.SetVector("_PosWorld", _mask.position);
+            _waterMat.SetVector("_ObjectScale", transform.localScale);
+        }
+    }
+
+    //Cập nhật màu
+    void UpdateColorsOnBottle()
+    {
+        _waterMat.SetColor("_Color1", bottleColors[0]);
+        _waterMat.SetColor("_Color2", bottleColors[1]);
+        _waterMat.SetColor("_Color3", bottleColors[2]);
+        _waterMat.SetColor("_Color4", bottleColors[3]);
+    }
+
+    void DoNuoc()
+    {
+        transform.DOKill();
+        transform.eulerAngles = Vector3.zero;
+        Vector3 angle = new Vector3(0, 0, 90);
+        transform.DORotate(angle, time, RotateMode.FastBeyond360).OnUpdate(() =>
+        {
+            if (transform.eulerAngles.z >= 30f && transform.eulerAngles.z < 31f) // Kiểm tra khoảng nhỏ để tránh gọi nhiều lần
+            {
+                Debug.Log("Góc đạt 30 độ!");
+                // Thực hiện hành động mong muốn
+                DOTween.To(() => 0.34f, x => _waterMat.SetFloat("_FillAmount", x), -0.5f, time);
+            }
+        });
+        DOTween.To(() => 1f, x => _scaleOffset = x, 0.5f, time);
+        DOTween.To(() => 1f, x => _waterMat.SetFloat("_ScaleOffset", x), 0.5f, time);
+
+    }
+
+    //0 : 0.34
+    //30 : 0.34
+    //54 : 0.13
+    //71 : -0.08
+    //83 : -0.29
+    //90 : -0.5
+
+    //IEnumerator DoNuoc()
+    //{
+    //    float t = 0;
+    //    float lerpValue;
+    //    float angleValue;
+
+    //    while(t < time)
+    //    {
+    //        lerpValue = t / time;
+    //        angleValue = Mathf.Lerp(0f, 90f, lerpValue);
+    //        _scaleOffset = Mathf.Lerp(1f, 0.5f, lerpValue);
+    //        transform.eulerAngles = new Vector3(0, 0, angleValue);
+    //        _waterMat.SetFloat("_ScaleOffset", _scaleOffset);
+    //        _waterMat.SetFloat("_FillAmount", _animationCurve.Evaluate(angleValue));
+    //        t += Time.deltaTime;
+
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //    angleValue = 90f;
+    //    transform.eulerAngles = new Vector3(0, 0, angleValue);
+    //    _waterMat.SetFloat("_ScaleOffset", _scaleOffset);
+    //    _waterMat.SetFloat("_FillAmount", _animationCurve.Evaluate(angleValue));
+
+    //}
+
+}
---
+++ Assets/Shaders/BottleShader.shader
@@ -0,0 +1,94 @@
+Shader "Unlit/Water"
+{
+    Properties
+    {
+        _MainTex ("Sprite Texture", 2D) = "white" {}
+        _Color1("Color1", Color) = (1, 1, 1, 1)
+        _Color2("Color2", Color) = (1, 1, 1, 1)
+        _Color3("Color3", Color) = (1, 1, 1, 1)
+        _Color4("Color4", Color) = (1, 1, 1, 1)
+        _Amount1("Amount1", Float) = -0.29
+        _Amount2("Amount2", Float) = -0.08
+        _Amount3("Amount3", Float) = 0.13
+        _Amount4("Amount4", Float) = 0.34
+        _FillAmount("FillAmount", Range(-0.5, 0.34)) = 0
+        _PosWorld("PosWorld", Vector) = (0, 0, 0, 0)
+        _ObjectScale("ObjectScale", Vector) = (0, 0, 0, 0)
+        _ScaleOffset("ScaleOffset", Float) = 1
+    }
+    SubShader
+    {
+        // Tags { "RenderType"="Opaque" }
+        // LOD 100
+
+        Pass
+        {
+            Tags
+            {
+                "Queue" = "Transparent"
+            }
+            Blend One OneMinusSrcAlpha
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv1 : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float4 vertex : POSITION;
+                float2 uv1 : TEXCOORD0;
+                float3 world_Pos : TEXCOORD1;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            float4 _Color1;
+            float4 _Color2;
+            float4 _Color3;
+            float4 _Color4;
+
+            float _Amount1;
+            float _Amount2;
+            float _Amount3;
+
+            float _FillAmount;
+            float4 _PosWorld;
+            float4 _ObjectScale;
+            float _ScaleOffset;
+
+            v2f vert (appdata i)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(i.vertex);
+                o.uv1 = i.uv1;
+                o.world_Pos = mul(unity_ObjectToWorld, i.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col;
+                float2 diff = (i.world_Pos - _PosWorld.xy)/_ObjectScale;
+                float uv = step(diff.y, _FillAmount*_ScaleOffset);
+
+                float s1 = step(diff.y, _Amount1*_ScaleOffset);
+                float s2 = step(diff.y, _Amount2*_ScaleOffset);
+                float s3 = step(diff.y, _Amount3*_ScaleOffset);
+
+                float4 c1 = lerp(_Color2, _Color1, s1);
+                float4 c2 = lerp(_Color3, c1, s2);
+                float4 c3 = lerp(_Color4, c2, s3);
+                col = c3 * uv;
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
